5c6997a1f9929818a9d0f28b3794290f3005daff
Sonya <sonyacisse7@gmail.com>
2025-08-06 14:39:06 -0400

    added submit name button and text box

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack { //start vstack
             Text("What is your name?")
                 .font(.title)
-        }
+            
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }//end button
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+        } //end vstack
         .padding()
     }
 }
